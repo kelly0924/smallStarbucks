c072e6d0e839b8529e73f61acb2ffcc0e6bfc1b8
kelly0924 <hyj09240@gmail.com>
2024-12-19 18:12:15 +0900

    feat(sql): data.sql 파일에 두개 더미 데이터 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 INSERT INTO MEMBER(name, email, nickname, password) VALUES('KIM', 'K123@naver.com', 'coco', 'KC1234!');
+INSERT INTO MEMBER(name, email, nickname, password) VALUES('Ham', 'H123@naver.com', 'hamham', 'Hh1234!');
+INSERT INTO MEMBER(name, email, nickname, password) VALUES('Lee', 'L345@naver.com', 'lee', 'Ll1234!');
