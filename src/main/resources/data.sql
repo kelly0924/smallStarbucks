c7dde35588075e6c3d739a24a6725de6ff19da8a
kelly0924 <hyj09240@gmail.com>
2024-12-20 11:53:59 +0900

    feat(data): 커피 테이블 더비 데이터 data.sql에 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,8 @@
 INSERT INTO MEMBER(name, email, nickname, password) VALUES('KIM', 'K123@naver.com', 'coco', 'KC1234!');
 INSERT INTO MEMBER(name, email, nickname, password) VALUES('Ham', 'H123@naver.com', 'hamham', 'Hh1234!');
 INSERT INTO MEMBER(name, email, nickname, password) VALUES('Lee', 'L345@naver.com', 'lee', 'Ll1234!');
+
+
+INSERT INTO COFFEE(menu, details, calories) VALUES('아메리카노', '원두의 향이 진하게 느껴지는', '20 칼로리');
+INSERT INTO COFFEE(menu, details, calories) VALUES('오늘의 커피', '매일 다른 원두로 만나 보세요', '20 칼로리');
+INSERT INTO COFFEE(menu, details, calories) VALUES('바닐라 라떼', '달달한 바닐라 시럽과 커피의 향이 가득한 ', '20 칼로리');
