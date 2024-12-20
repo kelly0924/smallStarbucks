f544e934afd0911f511f8dde1bf2eac930e081c3
kelly0924 <hyj09240@gmail.com>
2024-12-20 15:02:47 +0900

    feat(repository): CoffeeRepository interface 추가

--- src/main/java/com/example/small_starbucks/repository/CoffeeRepository.java
+++ src/main/java/com/example/small_starbucks/repository/CoffeeRepository.java
@@ -1,2 +1,7 @@
-package com.example.small_starbucks.repository;public interface CoffeeRepository {
+package com.example.small_starbucks.repository;
+
+import com.example.small_starbucks.entity.Coffee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoffeeRepository extends JpaRepository<Coffee, Long> {
 }
