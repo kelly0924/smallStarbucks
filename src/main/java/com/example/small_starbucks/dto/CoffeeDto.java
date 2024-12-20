12df76b6f1e312fabbf58c71d0bb4a61f99e30b7
kelly0924 <hyj09240@gmail.com>
2024-12-20 14:59:33 +0900

    feat(dto): coffee DTO 객체 추가

--- src/main/java/com/example/small_starbucks/dto/CoffeeDto.java
+++ src/main/java/com/example/small_starbucks/dto/CoffeeDto.java
@@ -1,2 +1,35 @@
-package com.example.small_starbucks.dto;public class CoffeeDto {
+package com.example.small_starbucks.dto;
+
+import com.example.small_starbucks.entity.Coffee;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CoffeeDto {
+    private Long coffee_index;
+    private String menu;
+    private String details;
+    private  String calories;
+
+    //dto ->entity로 변환 하기
+    public Coffee toEntity(){
+        return new Coffee(coffee_index, menu, details, calories);
+    }
+
+    //entity->dto로 변환 하기
+    public static CoffeeDto toDtio(Coffee entity){
+        return new CoffeeDto(
+                entity.getCoffee_index(),
+                entity.getMenu(),
+                entity.getDetails(),
+                entity.getCalories()
+        );
+
+    }
+
 }
