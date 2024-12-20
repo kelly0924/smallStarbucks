b19bb3b2c63c7488884d6f93834b59e9b8c9434a
kelly0924 <hyj09240@gmail.com>
2024-12-20 15:03:57 +0900

    feat(service): CoffeeService class 생성, coffee 메뉴 save()추가

--- src/main/java/com/example/small_starbucks/service/CoffeeService.java
+++ src/main/java/com/example/small_starbucks/service/CoffeeService.java
@@ -1,2 +1,25 @@
-package com.example.small_starbucks.service;public class CoffeeService {
+package com.example.small_starbucks.service;
+
+import com.example.small_starbucks.dto.CoffeeDto;
+import com.example.small_starbucks.entity.Coffee;
+import com.example.small_starbucks.repository.CoffeeRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class CoffeeService {
+    @Autowired
+    private CoffeeRepository coffeeRepository;
+
+    public CoffeeDto save(CoffeeDto dto) {
+        //dto -> entity로 변환
+        Coffee entity = dto.toEntity();
+        log.info("객체", dto.toEntity());
+        log.info("enity: ", entity.toString());
+        Coffee coffeeEntity = coffeeRepository.save(entity);
+        CoffeeDto saved = CoffeeDto.toDtio(coffeeEntity);
+        return saved;
+    }
 }
