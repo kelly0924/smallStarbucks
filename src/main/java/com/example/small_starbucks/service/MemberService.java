39b497926588cc41cdd3842fc2eb0dee1b04a880
kelly0924 <hyj09240@gmail.com>
2024-12-20 11:05:21 +0900

    feat(service): 회원정보 수정 update() 추가

--- src/main/java/com/example/small_starbucks/service/MemberService.java
+++ src/main/java/com/example/small_starbucks/service/MemberService.java
@@ -4,6 +4,8 @@ import com.example.small_starbucks.dto.Memberdto;
 import com.example.small_starbucks.entity.Member;
 import com.example.small_starbucks.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -25,5 +27,19 @@ public class MemberService {
         }
         Memberdto dto = entity.toDto(entity);
         return dto;
+    }
+
+    public Memberdto update(Long accountIndex, Memberdto dto) {
+        //dto->entity로 변환
+        Member entity = dto.toEntity();
+        Member targetEntity = memberRepository.findById(accountIndex).orElse(null);
+        //수정하고자 하는 회원이 존재 하지 않을 경우 예외 처리
+        if(accountIndex != entity.getAccount_id() || targetEntity.getAccount_id() == null) {
+            throw new IllegalArgumentException("id가 존재하지 않습니다.");
+        }
+        Member updateData = memberRepository.save(entity);
+        Memberdto updatedMember = updateData.toDto(updateData);
+
+        return updatedMember;
     }
 }
