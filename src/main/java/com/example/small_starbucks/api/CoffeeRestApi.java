e62d5a723c900121d7a24465100dfb31a87443c9
kelly0924 <hyj09240@gmail.com>
2024-12-20 15:59:13 +0900

    feat(api): coffeeList 전체를 가져오는 api 추가

--- src/main/java/com/example/small_starbucks/api/CoffeeRestApi.java
+++ src/main/java/com/example/small_starbucks/api/CoffeeRestApi.java
@@ -1,15 +1,19 @@
 package com.example.small_starbucks.api;
 
 import com.example.small_starbucks.dto.CoffeeDto;
+import com.example.small_starbucks.dto.Memberdto;
 import com.example.small_starbucks.entity.Coffee;
 import com.example.small_starbucks.service.CoffeeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -24,4 +28,13 @@ public class CoffeeRestApi {
         CoffeeDto coffeeDto = coffeeService.save(dto);
         return ResponseEntity.status(HttpStatus.OK).body(coffeeDto);
     }
+
+    //커피 메뉴 전체 가져오기
+    @GetMapping("/coffeelist")
+    public ResponseEntity<List<CoffeeDto>> coffeeList(){
+       List<CoffeeDto> listData = coffeeService.showList();
+        return ResponseEntity.status(HttpStatus.OK).body(listData);
+    }
+
+
 }
