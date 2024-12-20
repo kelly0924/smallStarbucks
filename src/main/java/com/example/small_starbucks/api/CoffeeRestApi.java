c0b5edbb04e003e7a337c33293da5353e1e8b0e8
kelly0924 <hyj09240@gmail.com>
2024-12-20 15:01:41 +0900

    feat(api): coffeeRestApi 생성, coffee 메뉴 저장하기  api 추가

--- src/main/java/com/example/small_starbucks/api/CoffeeRestApi.java
+++ src/main/java/com/example/small_starbucks/api/CoffeeRestApi.java
@@ -1,2 +1,27 @@
-package com.example.small_starbucks.api;public class CoffeeRestApi {
+package com.example.small_starbucks.api;
+
+import com.example.small_starbucks.dto.CoffeeDto;
+import com.example.small_starbucks.entity.Coffee;
+import com.example.small_starbucks.service.CoffeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class CoffeeRestApi {
+
+    @Autowired
+    private CoffeeService coffeeService;
+    //커피 메뉴를 저장하는 api
+    @PostMapping("/coffee")
+    public ResponseEntity<CoffeeDto> save(@RequestBody CoffeeDto dto){
+        log.info("요청에서?: ", dto.toEntity());
+        CoffeeDto coffeeDto = coffeeService.save(dto);
+        return ResponseEntity.status(HttpStatus.OK).body(coffeeDto);
+    }
 }
