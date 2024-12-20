d2e922ab8cf8ce1a26809c5c613bf667a64ab66a
kelly0924 <hyj09240@gmail.com>
2024-12-20 15:56:37 +0900

    feat(entity): coffee entity 기본 생성자 추가

--- src/main/java/com/example/small_starbucks/entity/Coffee.java
+++ src/main/java/com/example/small_starbucks/entity/Coffee.java
@@ -3,12 +3,14 @@ package com.example.small_starbucks.entity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Entity
 @ToString
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class Coffee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
