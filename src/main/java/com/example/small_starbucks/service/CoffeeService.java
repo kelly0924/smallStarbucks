9e7b852d3d536a89c301975fc7da2d4a45389d90
kelly0924 <hyj09240@gmail.com>
2024-12-20 16:00:11 +0900

    feat(service): coffee전체를 가져오는 showList()추가

--- src/main/java/com/example/small_starbucks/service/CoffeeService.java
+++ src/main/java/com/example/small_starbucks/service/CoffeeService.java
@@ -1,11 +1,14 @@
 package com.example.small_starbucks.service;
 
 import com.example.small_starbucks.dto.CoffeeDto;
+import com.example.small_starbucks.dto.Memberdto;
 import com.example.small_starbucks.entity.Coffee;
 import com.example.small_starbucks.repository.CoffeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
 @Service
@@ -21,5 +24,12 @@ public class CoffeeService {
         Coffee coffeeEntity = coffeeRepository.save(entity);
         CoffeeDto saved = CoffeeDto.toDtio(coffeeEntity);
         return saved;
+    }
+
+    public List<CoffeeDto> showList() {
+        List<Coffee> entityList = coffeeRepository.findAll();
+        log.info("entityList: " ,entityList.stream().toList());
+        List<CoffeeDto> dtoList =CoffeeDto.toDtoList(entityList);
+        return dtoList;
     }
 }
